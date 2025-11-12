29b2a51a1194c08e3dd6a2e9e458cfcaffa78a41
fearlesslyh <2924395978@qq.com>
2025-11-12 22:44:56 +0800

    1

--- code-mother/openapi2ts.config.ts
+++ code-mother/openapi2ts.config.ts
@@ -1,28 +1,5 @@
 export default {
   schemaPath: 'http://localhost:8123/api/v3/api-docs',
   serversPath: './src/api',
-  requestLibPath: "import { createOpenApiRequest as request } from '@/api/openapi';",
-  requestOptionsType: 'OpenApiGeneratorOptions',
-  namespace: false,
-  useStaticMethod: true,
-  projectName: 'codeMother',
-  mock: false,
-  hook: {
-    customFunctionName(path: string, method: string | undefined) {
-      const normalizedMethod = typeof method === 'string' ? method.toLowerCase() : 'get'
-      if (typeof path !== 'string' || path.length === 0) {
-        return normalizedMethod
-      }
-      const formattedSegments = path
-        .split('/')
-        .filter(Boolean)
-        .map((segment) =>
-          segment
-            .replace(/\{(.*)\}/, '$1')
-            .replace(/(^|[-_])(\w)/g, (_, __, letter) => letter.toUpperCase()),
-        )
-        .join('')
-      return `${normalizedMethod}${formattedSegments}`
-    },
-  },
+  requestLibPath: "import { createOpenApiRequest as request } from '@/api/openapi';"
 }
